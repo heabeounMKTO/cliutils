fda444a298b223ffbed041af05332c5c86e5e52b
heabeounMKTO <heabeoun69zin@gmail.com>
2023-08-02 10:51:03 +0700

    feat : shitposting2

--- quick_shitpost_gen/main.py
+++ quick_shitpost_gen/main.py
@@ -6,8 +6,10 @@ ap.add_argument("-i", "--image", required=True, help="path to input image")
 ap.add_argument("-t", "--title", required=True, help="title of your shitpost")
 ap.add_argument("-tt", "--toptext", required=False, help="top text")
 ap.add_argument("-bt", "--bottomtext", required=False, help="bottom text")
-ap.add_argument("-df", "--deep-fry", required=False, help="deep fries your image")
+ap.add_argument("-df", "--deepfry",action='store_true', help="deep fries your image")
 args = vars(ap.parse_args())
-
+print(args)
 sp = shitpost(args["title"], args["image"])
-sp.calculate_textPos()
+if args["deepfry"]:
+    sp.deep_fry()
+
